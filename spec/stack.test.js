373aa627a7e8d41858a81b201d9be2f5795d4bf1
nicosesma <nicosm310@gmail.com>
2017-02-13 18:09:04 -0800

    Stack implementation

--- spec/stack.test.js
+++ spec/stack.test.js
@@ -16,9 +16,73 @@ describe('Stack', () => {
   context('push()', () => {
     it('pushes an element to the top of the stack.', () => {
       const myStack = new Stack()
+      const otherStack = new Stack('one')
+      const thirdStack = new Stack([{item11: 'item1'}, {item22: 'item2'}])
+      const quarterStack = new Stack({key1: 'value1'})
 
       expect(() => myStack.push('foo'))
         .to.alter(() => myStack.length(), { from: 0, to: 1 })
+      expect(myStack.items).to.eql(['foo'])
+
+      expect(() => otherStack.push(['bar', 'baz']))
+        .to.alter(() => otherStack.length(), { from: 1, to: 3 })
+      expect(otherStack.items).to.eql(['bar', 'baz', 'one'])
+
+      expect(() => thirdStack.push('hello'))
+        .to.alter(() => thirdStack.length(), { from: 2, to: 3 })
+      expect(thirdStack.items).to.eql([
+        'hello',
+        {item11: 'item1'},
+        {item22: 'item2'}
+      ])
+
+      expect(() => quarterStack.push(['world']))
+        .to.alter(() => quarterStack.length(), { from: 1, to: 2 })
+      expect(quarterStack.items).to.eql(['world', {key1: 'value1'}])
+    })
+  })
+
+  context('pop()', () => {
+    it('returns and removes the top element of the stack or null if the stack is empty', () => {
+      const myStack = new Stack(['item1', 'item2'])
+      const otherStack = new Stack()
+
+      expect(() => myStack.pop())
+        .to.alter(() => myStack.length(), { from: 2, to: 1 })
+
+      expect(myStack.pop()).to.eql('item2')
+
+      expect(otherStack.peek()).to.be.null
+    })
+  })
+
+  context('peek()', () => {
+    it('returns the top element in the stack or null if the stack is empty', () => {
+      const myStack = new Stack('hello world!')
+      const otherStack = new Stack()
+
+      expect(myStack.peek()).to.eql('hello world!')
+      expect(otherStack.peek()).to.be.null
+    })
+  })
+
+  context('isEmpty()', () => {
+    it('returns true if the stack is empty or false if not', () => {
+      const myStack = new Stack('hello world!')
+      const emptyStack = new Stack()
+
+      expect(myStack.isEmpty()).to.eql(false)
+      expect(emptyStack.isEmpty()).to.eql(true)
+    })
+  })
+
+  context('length()', () => {
+    it('returns the number on items in the stack', () => {
+      const myStack = new Stack('hello world!')
+      const emptyStack = new Stack()
+
+      expect(myStack.length()).to.eql(1)
+      expect(emptyStack.length()).to.eql(0)
     })
   })
 })
--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,60 @@
 'use strict'
 
 export default class Stack {
-  // your code here
+  constructor(instantiator) {
+    if (!(typeof instantiator === 'undefined')) {
+      if (!Array.isArray(instantiator)) {
+        this.items = [instantiator]
+        this.size = 1
+      }
+      if (Array.isArray(instantiator)) {
+        this.items = instantiator
+        this.size = instantiator.length
+      }
+    } else {
+      this.items = []
+      this.size = 0
+    }
+  }
+
+
+  push(item) {
+    if (Array.isArray(item)) {
+      this.size = this.size + item.length
+    }
+    if (!(Array.isArray(item))) {
+      item = [item]
+      this.size ++
+    }
+
+    this.items = [...item, ...this.items]
+    return this.items
+  }
+
+
+  pop() {
+    if (this.length === 0) {
+      return null
+    }
+
+    this.size --
+    return this.items.splice(0, 1)[0]
+  }
+
+
+  peek() {
+    return this.size
+      ? this.items[0]
+      : null
+  }
+
+
+  isEmpty() {
+    return this.size === 0
+  }
+
+
+  length() {
+    return this.size
+  }
 }
